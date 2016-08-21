4a9a641888886634d84afd69ba9f7c777cc0e2ed
Bad Dragon <bd42@debian>
2016-08-21 17:27:48 +0200

    Minor changes

--- render.cpp
+++ render.cpp
@@ -3,14 +3,13 @@
 
 #include <math.h>
 
-#define OBJLEN 512
+#define OBJLEN 1024
 ObjectGL* objects[OBJLEN];
 
 #define OBJX 0.5f
-#define OBJY 4.5f
-#define OBJYHALF 2.25f
+#define OBJY 5.0f
+#define OBJYHALF 2.5f
 #define OBJZ 5.0f
-#define OBJSIZE 0.04f
 
 double timePassed = 0;
 
@@ -24,18 +23,15 @@ void render(const double deltaTime)
 		{
 			float _x = ((i % 8) - 4) * OBJX + OBJX / 2;
 			float _y = (i / 8) * (OBJY / (OBJLEN / 8)) - OBJYHALF;
-			objects[i] = new CubeGL(_x, _y, -OBJZ, OBJSIZE, OBJSIZE, OBJSIZE);
+			objects[i] = new CubeGL(_x, _y, -OBJZ, 0.02f, 0.01f, 0.02f);
 		}
 	}
 
 	for (int i = 0; i < OBJLEN; i++)
 	{
 		objects[i]->Move(0.0f, 1.0f * -deltaTime, 0.0f);
-		
 		while(objects[i]->GetY() < -OBJYHALF)
-		{
-			objects[i]->Move(0.0f, OBJY, 0.0f);
-		}
+		{ objects[i]->Move(0.0f, OBJY, 0.0f); }
 	}
 
 	for (int i = 0; i < OBJLEN; i++)
